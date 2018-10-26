bc54e4f4e88c837cd521e69d56468dcbcd24e4c6
Christoph Bussenius <christoph-dev@bussenius.org>
2018-10-26 12:17:51 +0200

    Add a feature to exclude moves from the search.
    The new GTP command "avoid" adds moves to the list of moves to avoid.
    Syntax is "avoid <color> <coords> <movenum>".
    The specified coords will not be considered for this color starting from
    the current move until the specified move number. A "+" in front of the
    move number makes it relative to the current move number.
    
    Example: "avoid b q16 +1" forbids Black to play q16 at the next move.

--- src/FastState.h
+++ src/FastState.h
@@ -26,6 +26,17 @@
 
 #include "FullBoard.h"
 
+struct MoveToAvoid {
+    int color;
+    size_t from_move, to_move;
+    int vertex;
+
+    bool operator==(const MoveToAvoid other) const {
+        return color == other.color && from_move == other.from_move &&
+            to_move == other.to_move && vertex == other.vertex;
+    }
+};
+
 class FastState {
 public:
     void init_game(int size, float komi);
@@ -34,6 +45,7 @@ public:
 
     void play_move(int vertex);
     bool is_move_legal(int color, int vertex) const;
+    bool is_to_avoid(int color, int vertex, size_t movenum) const;
 
     void set_komi(float komi);
     float get_komi() const;
@@ -44,6 +56,7 @@ public:
     void set_to_move(int tomove);
     void set_passes(int val);
     void increment_passes();
+    void add_move_to_avoid(int color, int vertex, size_t from_move, size_t to_move);
 
     float final_score() const;
     std::uint64_t get_symmetry_hash(int symmetry) const;
@@ -61,6 +74,7 @@ public:
     int m_komove;
     size_t m_movenum;
     int m_lastmove;
+    std::vector<MoveToAvoid> m_moves_to_avoid;
 
 protected:
     void play_move(int color, int vertex);
